767fbda9385798439a7365af28d3561aca71d4c7
Jil-Lehnert <jillehnert@gmail.com>
2024-06-17 14:45:09 +0200

    creating config

--- caaml_to_ini.py
+++ caaml_to_ini.py
@@ -1,28 +1,37 @@
 import numpy as np
 from pathlib import Path
 import snowmicropyn
-
-# Warum funktioniert es nur so? -> später lösen!
-try:
-    from snowpyt import pit_class as pc
-except:
-    pass
+from snowpyt import pit_class as pc
 from snowpyt import CAAMLv6_xml as ca
+import yaml
 
 
-pnt_caaml_allocation_filename = "pnt_caaml_allocation.csv"
-matching = 'scaling'
+#pnt_caaml_allocation_filename = "pnt_caaml_allocation.csv"
+#matching = 'scaling'
 #matching = 'cutting'
-subdir_csv = "/data/"
+#subdir_csv = "/data/"
+#subdir_pnt = "/data/smp_pnt_files_scaled/"
 
 parentdir = Path(__file__).parent.as_posix()
+
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+config_path = 'config.yaml'
+config = load_config(config_path)
+    
+ # Zugriff auf die Konfigurationswerte
+pnt_path = config['pnt_path']
+scaling = config['matching']
+
 
 def caaml_filename_allocator (pnt_filename, pnt_caaml_allocation_filename):
     '''
     This function finds the allocating caaml filename to the given pnt filename
     in the given csv table.
     '''
-    subdir_csv = "/data/"
     pnt_caaml_allocation = np.loadtxt(parentdir + subdir_csv + pnt_caaml_allocation_filename, skiprows=1, delimiter=';', dtype='str')
     itemindex = np.where(pnt_caaml_allocation == pnt_filename)
     if len(itemindex[0]) > 1:
@@ -34,38 +43,36 @@ pnt_name_list = np.loadtxt(parentdir + subdir_csv + pnt_caaml_allocation_filenam
 for pnt_filename in pnt_name_list:
 
     caaml_filename = caaml_filename_allocator(pnt_filename, pnt_caaml_allocation_filename)
-    subdir_pnt = "/data/smp_pnt_files_scaled/"
-    #subdir_pnt = "/data/smp_pnt_files_cutted/"
 
     print(parentdir + subdir_pnt + pnt_filename)
-    p = snowmicropyn.Profile.load(parentdir + subdir_pnt + pnt_filename)
+    current_profile = snowmicropyn.Profile.load(parentdir + subdir_pnt + pnt_filename)
 
-    markers = p.markers
+    markers = current_profile.markers
     if markers:
-        surface = p.marker('surface')
-        ground = p.marker('ground')
+        surface = current_profile.marker('surface')
+        ground = current_profile.marker('ground')
 
     else:
-        surface = p.detect_surface()
-        ground = p.detect_ground()
-        p.save()
+        surface = current_profile.detect_surface()
+        ground = current_profile.detect_ground()
+        current_profile.save()
 
     subdir_caaml = '/data/caaml_files/'
     caaml_layers = ca.get_layers(parentdir + subdir_caaml + caaml_filename)
 
     ini_depth = ground - surface
     caaml_top = caaml_layers[0].dtop*10
-    caaml_depth = (caaml_layers[0].dtop - caaml_layers[-1].dbot)*10
+    caaml_depth = (caaml_layers[0].dtop - caaml_layers[-1].dbot)*10 #warum *10
 
     # factor to scale the depth of the CAAML profile to that of the SMP measurement
     #TODO - at a later point add option without scaling, insteadt cut off CAAMML
     correction_factor = ini_depth / caaml_depth
 
-    if matching == "cutting":        
+    if matching == "cutting":
         if ini_depth >= caaml_depth:
             ground = surface + caaml_depth
-            p.remove_marker('ground')
-            p.set_marker('ground', ground)
+            current_profile.remove_marker('ground')
+            current_profile.set_marker('ground', ground)
 
     i = 0
     for layer in caaml_layers:
@@ -75,16 +82,17 @@ for pnt_filename in pnt_name_list:
         i = i + 1
         if matching == "scaling":
             layer_bottom = (caaml_top - layer.dbot*10)*correction_factor + surface
-            p.set_marker(graintype, layer_bottom)
+            current_profile.set_marker(graintype, layer_bottom)
+            print(graintype, layer_bottom)
         elif matching == "cutting":
             layer_bottom = (caaml_top - layer.dbot*10) + surface
             if caaml_depth > ini_depth:
                 if layer_bottom < ground:
-                    p.set_marker(graintype, layer_bottom)
+                    current_profile.set_marker(graintype, layer_bottom)
                 else:
-                    p.set_marker(graintype, ground)
+                    current_profile.set_marker(graintype, ground)
                     break
             elif ini_depth >= caaml_depth:
-                p.set_marker(graintype, layer_bottom)
-    p.save()
+                current_profile.set_marker(graintype, layer_bottom)
+    current_profile.save()
 
