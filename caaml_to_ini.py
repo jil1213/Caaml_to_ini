c9861f9bb27da68be4d537ceeff92c75311c4e02
Jil-Lehnert <jillehnert@gmail.com>
2024-06-26 09:49:32 +0200

    making code clearer

--- caaml_to_ini.py
+++ caaml_to_ini.py
@@ -7,6 +7,10 @@ import yaml
 
 
 def load_config(file_path):
+    '''
+    Load config.yaml to get the input parameter 
+    file_path(str): path to the config.yaml file 
+    '''
     with open(file_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
@@ -16,6 +20,8 @@ def caaml_filename_allocator (pnt_filename, pnt_caaml_allocation):
     '''
     This function finds the allocating caaml filename to the given pnt filename
     in the given csv table.
+    pnt_filename(str): name of the pnt file
+    pnt_caaml_allocation(str): 
     '''
     allocation = np.loadtxt(pnt_caaml_allocation, skiprows=1, delimiter=';', dtype='str')
     itemindex = np.where(allocation == pnt_filename)
@@ -23,12 +29,17 @@ def caaml_filename_allocator (pnt_filename, pnt_caaml_allocation):
         raise ValueError("PNT filename existing more than one time in CSV table")
     return str(allocation[itemindex[0],1][0])
 
-def caaml_to_ini(pnt_caaml_allocation, matching, pnt_path, caaml_path):
+
+def caaml_to_ini(pnt_caaml_allocation, pnt_path, caaml_path, scaling=False):
     '''
     This function reads the SMP profiles and the corresponding CAAML profiles
     and matches the layers of the CAAML profile to the SMP profile.
     The matching can be done by cutting the CAAML profile at the depth of the SMP profile
     or by scaling the depth of the CAAML profile to the depth of the SMP profile.
+    pnt_caaml_allocation(str): path to the csv table with the allocation of the SMP profiles to the CAAML profiles
+    pnt_path(str): path to the SMP profiles
+    caaml_path(str): path to the CAAML profiles
+    scaling(bool): Default False(Profiles are getting cutted), if True(profiles are getting scaled)
     '''
     pnt_name_list = np.loadtxt(pnt_caaml_allocation, skiprows=1, delimiter=';', dtype='str')[:,0].tolist()
 
@@ -59,7 +70,7 @@ def caaml_to_ini(pnt_caaml_allocation, matching, pnt_path, caaml_path):
         #TODO - at a later point add option without scaling, insteadt cut off CAAMML
         correction_factor = ini_depth / caaml_depth
 
-        if matching == "cutting":
+        if scaling:
             if ini_depth >= caaml_depth:
                 ground = surface + caaml_depth
                 current_profile.remove_marker('ground')
@@ -71,11 +82,11 @@ def caaml_to_ini(pnt_caaml_allocation, matching, pnt_path, caaml_path):
             #numbers are deleted later on in the SNOWDRAGON package
             graintype = layer.grain_type1 + str(i)
             i = i + 1
-            if matching == "scaling":
+            if scaling:
                 layer_bottom = (caaml_top - layer.dbot*10)*correction_factor + surface
                 current_profile.set_marker(graintype, layer_bottom)
                 print(graintype, layer_bottom)
-            elif matching == "cutting":
+            elif scaling == False:
                 layer_bottom = (caaml_top - layer.dbot*10) + surface
                 if caaml_depth > ini_depth:
                     if layer_bottom < ground:
@@ -88,14 +99,13 @@ def caaml_to_ini(pnt_caaml_allocation, matching, pnt_path, caaml_path):
         current_profile.save()
 
 def main():
-    config_path = 'config.yaml'
-    config = load_config(config_path)
+    config = load_config('config.yaml')
     #Load configs
     pnt_caaml_allocation = config['input_data']['pnt_caaml_allocation']
     pnt_path = config['input_data']['pnt_path']
     caaml_path = config['input_data']['caaml_path']
-    matching = config['matching']
-    caaml_to_ini(pnt_caaml_allocation, matching, pnt_path, caaml_path)
+    scaling = config['scaling']
+    caaml_to_ini(pnt_caaml_allocation, pnt_path, caaml_path, scaling)
 
 
 if __name__ == "__main__":
